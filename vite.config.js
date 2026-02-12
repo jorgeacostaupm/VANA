0344c16c1223d5622e8d563d1976fbbdc0d6ebed
Jorge <jorge.acosta@upm.es>
2026-02-12 11:12:41 +0100

    solving paths on deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/VIANA/",
+  base: process.env.VITE_BASE_PATH || "/VIANNA/",
   plugins: [react()],
   resolve: {
     alias: {
